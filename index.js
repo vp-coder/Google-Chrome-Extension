b059f87d69b62f8824d83c664c9c97720502520a
Vidushi Pandey <viduvibhu2@gmail.com>
2021-11-27 15:28:09 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const url='https://icanhazdadjoke.com/slack';
-fetch(url)
-.then(data=>data.json())
-.then(jokeData=>{
-    const jokeText=jokeData.attachments[0].text;
-    const jokeElement=document.getElementById('jokeElement');
-    jokeElement.innerHTML=jokeText;
-})
